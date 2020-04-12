cf866e6cb0e1aca4b5349b6b3990add98a63dc48
Gilvan Muniz <gilvanmunizpa@gmail.com>
2020-04-12 17:17:26 -0300

    Bomdia gilvan oi

--- Aula97ap/Program.cs
+++ Aula97ap/Program.cs
@@ -7,6 +7,7 @@ namespace Aula97ap
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia Gilvan oi");
         }
     }
 }
