6e275f1830b74c893af65058e67ca22a92e7e3bb
Gilvan Muniz <gilvanmunizpa@gmail.com>
2020-04-13 10:04:32 -0300

    meu bom dia

--- Aula97ap/Program.cs
+++ Aula97ap/Program.cs
@@ -10,6 +10,7 @@ namespace Aula97ap
             Console.WriteLine("Bom dia Gilvan oi");
             Console.WriteLine("Gilvan como vc esta?");
             Console.WriteLine("amigo já é 23:00 hs");
+            Console.WriteLine("bom dia 09:49hs");
         }
     }
 }
