df3b198aa4adafcc56a388e5a2d8da86968ed678
Gilvan Muniz <gilvanmunizpa@gmail.com>
2020-04-13 10:51:22 -0300

    Excclui uma linha

--- Aula97ap/Program.cs
+++ Aula97ap/Program.cs
@@ -7,7 +7,6 @@ namespace Aula97ap
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia Gilvan oi");
             Console.WriteLine("Gilvan como vc esta?");
             Console.WriteLine("amigo já é 23:00 hs");
             Console.WriteLine("bom dia 09:49hs");
