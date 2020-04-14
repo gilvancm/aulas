ca46cfe4c3d7fa831214374cf67464ea04da835e
Gilvan Muniz <gilvanmunizpa@gmail.com>
2020-04-13 22:13:28 -0300

    sistema pedido ver 1

---
+++ Aula115ap/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aula115ap.Entities.Enums;
+
+namespace Aula115ap.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return  Id +
+                   " , "
+                   + Moment +
+                   ","
+                   + Status;
+
+        }
+    }
+}
---
+++ Aula115ap/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Aula115ap.Entities;
+using Aula115ap.Entities.Enums;
+
+namespace Aula115ap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+            Console.WriteLine();
+            
+            //ccoverte de enumerado pra string
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine("item teliçence"); 
+            Console.WriteLine("conversão de enumeração pra string e de string pra enumeração");
+            //     vou parametrizar como se fosse uma lista
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            
+            Console.WriteLine("imprime emurerado no formato de string    "+txt);
+            Console.WriteLine();
+            Console.WriteLine("imprime do formato string para o enumerado    "+os);
+
+
+        }
+    }
+}
